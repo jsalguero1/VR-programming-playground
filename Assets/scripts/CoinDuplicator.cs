4e58ede3ef769350ab5b1390bbfbec09fcd7e3df
Juan David Salguero Medina <j.salguero@uniandes.edu.co>
2024-11-15 11:13:16 -0500

    Update CoinDuplicator.cs

--- Assets/scripts/CoinDuplicator.cs
+++ Assets/scripts/CoinDuplicator.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class CoinDuplicator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject coinPrefab; // Prefab de la moneda (debe incluir XRGrabInteractable y Rigidbody)
+    [SerializeField] private Transform spawnPoint;  // Punto donde se creará la moneda duplicada
+
+    private void OnEnable()
     {
-        
+        // Asegúrate de que el XRGrabInteractable original esté deshabilitado
+        var grabInteractable = GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrabAttempt);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        var grabInteractable = GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.RemoveListener(OnGrabAttempt);
+        }
+    }
+
+    private void OnGrabAttempt(SelectEnterEventArgs args)
+    {
+        // Generar una copia de la moneda
+        GameObject coinCopy = Instantiate(coinPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        // Activar físicas para la copia
+        var coinRigidbody = coinCopy.GetComponent<Rigidbody>();
+        if (coinRigidbody != null)
+        {
+            coinRigidbody.isKinematic = false; // Habilitar físicas
+        }
+
+        // Hacer que la copia sea agarrable
+        var grabInteractable = coinCopy.GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null)
+        {
+            // Forzar que el jugador agarre la nueva moneda
+            var interactor = args.interactorObject;
+            grabInteractable.interactionManager.SelectEnter(interactor, grabInteractable);
+        }
     }
 }
