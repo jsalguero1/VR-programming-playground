6ba77d3e603d576d4bb7a41a7a026c49d323f695
Juan David Salguero Medina <j.salguero@uniandes.edu.co>
2024-11-21 16:45:08 -0500

    Update DropDownHandler.cs

--- Assets/scripts/DropDownHandler.cs
+++ Assets/scripts/DropDownHandler.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Usar esto si es un TMP_Dropdown
 
-public class dropDownHandler : MonoBehaviour
+public class DropdownHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Dropdown dropdown; // Referencia al Dropdown (o Dropdown normal si no usas TMP)
+
+    private void Start()
     {
-        
+        if (dropdown != null)
+        {
+            // Suscribimos el método al evento onValueChanged
+            dropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+        }
+        else
+        {
+            Debug.LogError("No se asignó un Dropdown al script.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDropdownValueChanged(int valueIndex)
     {
-        
+        // Imprimimos la opción seleccionada
+        string selectedOption = dropdown.options[valueIndex].text;
+        Debug.Log($"Opción seleccionada: {selectedOption}");
+    }
+
+    private void OnDestroy()
+    {
+        if (dropdown != null)
+        {
+            // Removemos el listener al destruir el objeto para evitar problemas
+            dropdown.onValueChanged.RemoveListener(OnDropdownValueChanged);
+        }
     }
 }
